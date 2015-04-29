cd4097b50f749d3fe0dca79b2c8fff982e396e3c
AxelMonroyX <xaxelmonroyx@gmail.com>
2015-04-29 10:11:36 -0700

    Creacion de clase Grupo
    Creación de la clase grupo con las propiedades de la base de datos,
    además de un método para obtener el ID del grupo

---
+++ SistemaExtraescolares/Modelos/Grupo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SistemaExtraescolares
+{
+    internal class Grupo
+    {
+        private int IDActividad { get; set; }
+
+        public Int32 IDGrupo { get; set; }
+
+        public Int32 CicloEscolar_Anho { get; set; }
+
+        public Int32 CicloEscolar_MesPrimero { get; set; }
+
+        public Int32 CicloEscolar_MesUltimo { get; set; }
+
+        public Int32 Capacidad { get; set; }
+
+        public string Horario { get; set; }
+
+        public Grupo()
+        {
+            IDGrupo = 0;
+            IDActividad = 0;
+
+            Horario = String.Empty;
+            Capacidad = 0;
+            CicloEscolar_Anho = 0;
+            CicloEscolar_MesPrimero = 0;
+            CicloEscolar_MesUltimo = 0;
+        }
+
+        public override String ToString()
+        {
+            return IDGrupo.ToString();
+        }
+    }
+}
