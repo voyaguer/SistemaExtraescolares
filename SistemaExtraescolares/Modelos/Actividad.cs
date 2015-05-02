4739f46b315099745c77170224980c00ddc115aa
AxelMonroyX <xaxelmonroyx@gmail.com>
2015-05-01 22:43:05 -0700

    Remover algunas librerias en la clase Actividad
    No se necesitaba exportar la mayoria de las librerias

--- SistemaExtraescolares/Modelos/Actividad.cs
+++ SistemaExtraescolares/Modelos/Actividad.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlClient;
 
 namespace SistemaExtraescolares
 {
-    class Actividad
+     class Actividad
     {
         public Int32 IDActividad { get; set; }
+
         public String Nombre { get; set; }
 
         public Actividad()
